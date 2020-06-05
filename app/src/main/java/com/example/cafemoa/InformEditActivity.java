59abc7686c7eff4043c3bd3a456bf422ec6bd0e7
gkdud4201 <63914662+gkdud4201@users.noreply.github.com>
2020-06-06 00:23:30 +0900

    review image,edit inform

---
+++ app/src/main/java/com/example/cafemoa/InformEditActivity.java
@@ -0,0 +1,14 @@
+package com.example.cafemoa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InformEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inform_edit);
+    }
+}
